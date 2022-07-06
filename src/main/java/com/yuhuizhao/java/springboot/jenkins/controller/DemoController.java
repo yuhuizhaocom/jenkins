134d222da8ae84a64f3f32e1e78519b2d84142ab
yuhuizhao <yuhuizhao@qq.com>
2022-07-06 23:08:18 +0800

    修改默认端口，增加demo地址映射

--- src/main/java/com/yuhuizhao/java/springboot/jenkins/controller/DemoController.java
+++ src/main/java/com/yuhuizhao/java/springboot/jenkins/controller/DemoController.java
@@ -1,8 +1,12 @@
 package com.yuhuizhao.java.springboot.jenkins.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class DemoController {
 
+    @GetMapping
     public String demo() {
 
         return "demo";
